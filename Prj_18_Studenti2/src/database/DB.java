dafbb9f565d0bb2d56902e30c35e70ae0a85d3eb
maboglia <mauro.bogliaccino@gmail.com>
2021-04-16 12:43:18 +0200

    addd comment: /* elenco nomi nascosto */

--- Prj_18_Studenti2/src/database/DB.java
+++ Prj_18_Studenti2/src/database/DB.java
@@ -54,25 +54,8 @@ public class DB {
 //		
 //		
 //		
-//		addStudente(new Studente("Amal",	"El Arabi", 2000), 0);
-//		addStudente(new Studente("Ayoub",	"Bettaieb",	2001), 1);
-//		addStudente(new Studente("Dario",	"Iapicca",	1995), 2);
-//		addStudente(new Studente("Davide",	"Beltramo",	1997), 3);
-//		addStudente(new Studente("Diego",	"Pellegrino",	1998), 4);
-//		addStudente(new Studente("Emanuele",	"Sandrone",	1996), 5);
-//		addStudente(new Studente("Federica",	"Corbezzolo",	1995), 6);
-//		addStudente(new Studente("giacomo",	"mosso",	1998), 7);
-//		addStudente(new Studente("Giada",	"Squillaci",	1993), 8);
-//		addStudente(new Studente("Luca",	"Basello",	1996), 9);
-//		addStudente(new Studente("Maryam",	"Alimohammadi",	1987), 10);
-//		addStudente(new Studente("Matteo",	"Tosin",	1994), 11);
-//		addStudente(new Studente("Mattia",	"Vazzoler",	1989), 12);
-//		addStudente(new Studente("Mirko",	"Franco",	1990), 13);
-//		addStudente(new Studente("Nabil",	"Bougarech",	1993), 14);
-//		addStudente(new Studente("Rebecca",	"Ghergo",	1996), 15);
-//		addStudente(new Studente("Salvatore",	"Zollino",	1994), 16);
-//		addStudente(new Studente("Tommaso",	"Angeli",	1999), 17);
-//		
+/		/* elenco nomi nascosto */
+	
 		//inizializzo i valori dell'AL
 //		for (Studente studente : studentiAR) {
 //			this.studentiAL.add(studente);
--- Prj_21_ComparaStudenti/src/database/DB.java
+++ Prj_21_ComparaStudenti/src/database/DB.java
@@ -18,24 +18,8 @@ public class DB {
 	}
 	
 	private void inizializza() {
-		this.studenti.add(new Studente("Amal",	"El Arabi", 2000));
-		this.studenti.add(new Studente("Ayoub",	"Bettaieb",	2001));
-		this.studenti.add(new Studente("Dario",	"Iapicca",	1995));
-		this.studenti.add(new Studente("Davide",	"Beltramo",	1997));
-		this.studenti.add(new Studente("Diego",	"Pellegrino",	1998));
-		this.studenti.add(new Studente("Emanuele",	"Sandrone",	1996));
-		this.studenti.add(new Studente("Federica",	"Corbezzolo",	1995));
-		this.studenti.add(new Studente("giacomo",	"mosso",	1998));
-		this.studenti.add(new Studente("Giada",	"Squillaci",	1993));
-		this.studenti.add(new Studente("Luca",	"Basello",	1996));
-		this.studenti.add(new Studente("Maryam",	"Alimohammadi",	1987));
-		this.studenti.add(new Studente("Matteo",	"Tosin",	1994));
-		this.studenti.add(new Studente("Mattia",	"Vazzoler",	1989));
-		this.studenti.add(new Studente("Mirko",	"Franco",	1990));
-		this.studenti.add(new Studente("Nabil",	"Bougarech",	1993));
-		this.studenti.add(new Studente("Rebecca",	"Ghergo",	1996));
-		this.studenti.add(new Studente("Salvatore",	"Zollino",	1994));
-		this.studenti.add(new Studente("Tommaso",	"Angeli",	1999));	
+		this.studenti.add(new Studente("nome",	"cognome", 2000));
+		/* elenco nomi nascosto */
 	
 	}
 	
